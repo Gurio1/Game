2f38a04c6ea5cce04fc2dba4496c272920da80f8
Andrey <iliev.andreyy@gmail.com>
2024-07-21 23:39:28 +0300

    Registered FastEndpoints,Serilog,IdentityModule,MediatR

--- Game.Web/Program.cs
+++ Game.Web/Program.cs
@@ -1,10 +1,36 @@
-using Game;
-using Game.Hubs;
+using System.Reflection;
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Game.Users;
+using Game.Web;
+using Game.Web.Hubs;
+using Serilog;
+
+var logger = Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+logger.Information("Starting web host");
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Host.UseSerilog((_, config) => 
+    config.ReadFrom.Configuration(builder.Configuration));
+
+builder.Services.AddFastEndpoints()
+    .AddAuthenticationJwtBearer(options =>
+    {
+        options.SigningKey = builder.Configuration["Auth:JwtSecret"]!;
+    })
+    .AddAuthorization()
+    .SwaggerDocument();
+
+// Add Module Services
+List<Assembly> mediatRAssemblies = [typeof(Program).Assembly];
+builder.Services.AddIdentityModuleServices(builder.Configuration, logger, mediatRAssemblies);
+
 builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
 {
@@ -28,10 +54,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
     };
 });
 
+builder.Services.AddSwaggerGen();
+
 
 builder.Services.AddScoped<CombatService>();
 
 var app = builder.Build();
+
+app.UseAuthentication()
+    .UseAuthorization();
+
+app.UseFastEndpoints()
+    .UseSwaggerGen();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
