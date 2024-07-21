f5d65e0fd55c05806e0278c43a28bcf1b0b23a86
Andrey <iliev.andreyy@gmail.com>
2024-07-21 23:39:43 +0300

    Added maxHp property

---
+++ Game.Web/Models/Monster.cs
@@ -0,0 +1,9 @@
+namespace Game.Web.Models;
+
+public class Monster
+{
+    public int MaxHp { get; set; }
+    public int CurrentHp { get; set; } 
+    public int Defence { get; set; } 
+    public int Damage { get; set; }
+}
--- Game/Models/Monster.cs
+++
@@ -1,8 +0,0 @@
-namespace Game.Models;
-
-public class Monster
-{
-    public int HP { get; set; } = 100;
-    public int Defence { get; set; } = 6;
-    public int Damage { get; set; } = 15;
-}
