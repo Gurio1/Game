b3e49d184f4b7bee235d8790797362e3c3e04393
Andrey <iliev.andreyy@gmail.com>
2024-07-22 22:53:35 +0300

    Configured routing between Register and Login components

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CombatComponent } from "./combat/combat.component";
+import { RegistrationComponent } from "./registration/registration.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CombatComponent],
+  imports: [RouterOutlet, CombatComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
