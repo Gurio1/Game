16a406b2867c1c79b1a766736321b27a8144081f
Andrey <iliev.andreyy@gmail.com>
2024-07-31 23:54:30 +0300

    Added logic for storing jwt token in local storage

--- ClientApp/src/constants.ts
+++ ClientApp/src/constants.ts
@@ -1,1 +1,2 @@
 export const API_URL = "https://localhost:7067/";
+export const JWT_TOKEN = "Jwt token"
--- ClientApp/src/services/identity.service.ts
+++ ClientApp/src/services/identity.service.ts
@@ -1,10 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 import { API_URL } from '../constants'
 import { User } from '../models/User';
 import { registerUser } from '../contracts/registerUser';
 import { loginUser } from '../contracts/loginUser';
+import { JWT_TOKEN } from '../constants';
+import { identityTokenResponse } from '../contracts/api-responses/identityTokenResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -13,12 +15,24 @@ export class IdentityService {
 
   constructor(private http: HttpClient) { }
 
-  registerUser(user : registerUser) : Observable<undefined>{
-    return this.http.post<undefined>(API_URL + `users`,user);
+  registerUser(user : registerUser) : Observable<identityTokenResponse>{
+    return this.http.post<identityTokenResponse>(API_URL + `users`,user)
+    .pipe(
+        tap((response: identityTokenResponse) => {
+          localStorage.setItem(JWT_TOKEN, response.token);
+        }),
+        catchError(this.handleError)
+      );;
   }
 
-  login(user : loginUser) : Observable<User>{
-    return this.http.post<User>(API_URL + `users/login`,user)
+  login(user : loginUser) : Observable<identityTokenResponse>{
+    return this.http.post<identityTokenResponse>(API_URL + `users/login`,user)
+    .pipe(
+        tap((response: identityTokenResponse) => {
+          localStorage.setItem(JWT_TOKEN, response.token);
+        }),
+        catchError(this.handleError)
+      );
   }
 
   isEmailTaken(email:string) : Observable<boolean>{
@@ -28,4 +42,18 @@ export class IdentityService {
   isUserNameTaken(userName: string) : Observable<boolean>{
     return this.http.get<boolean>(API_URL + `users`)
   }
+
+  private handleError(error: HttpErrorResponse) {
+  if (error.status === 0) {
+    // A client-side or network error occurred. Handle it accordingly.
+    console.error('An error occurred:', error.error);
+  } else {
+    // The backend returned an unsuccessful response code.
+    // The response body may contain clues as to what went wrong.
+    console.error(
+      `Backend returned code ${error.status}, body was: `, error.error);
+  }
+  // Return an observable with a user-facing error message.
+  return throwError(() => new Error('Something bad happened; please try again later.'));
+}
 }
