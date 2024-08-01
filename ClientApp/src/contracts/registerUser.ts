1c47e5a565e4f37f9bfd83430293639dd3f5b2c0
Andrey <iliev.andreyy@gmail.com>
2024-08-01 23:19:39 +0300

    Removed userName from registration

--- ClientApp/src/contracts/registerUser.ts
+++ ClientApp/src/contracts/registerUser.ts
@@ -1,3 +1,3 @@
 export class registerUser{
-    constructor(public userName: string, public email: string, public password: string, public confirmedPassword: string){}
+    constructor(public email: string, public password: string, public confirmedPassword: string){}
 }
