77204c3125b7d3551164897d406be81556237df4
Andrey <iliev.andreyy@gmail.com>
2024-08-02 23:36:17 +0300

    Added test variant for home page

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './identity/login/login.component';
 import { RegistrationComponent } from './identity/registration/registration.component';
+import { CreateCharacterComponent } from './create-character/create-character.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  {
+    path:'',
+    component:HomeComponent
+  //   path: '',
+  //   component: LayoutComponent,
+  //   children: [
+  //     { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //     { path: 'login', component: LoginComponent },
+  //     { path: 'register', component: RegistrationComponent }
+  //   ]
+  // },
+  // {path: 'create-character',component: CreateCharacterComponent
+  }
 ];
