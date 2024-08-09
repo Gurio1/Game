0c5adba19b0f328f6e0d9086f9293deef76e0335
Andrey <iliev.andreyy@gmail.com>
2024-08-09 23:06:56 +0300

    Added first prototype of home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,53 +1,23 @@
-import { NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-
-interface Item {
-  name: string;
-  img: string;
-  slot: string;
-}
-
-interface EquipmentSlot {
-  name: string;
-  item: Item | null;
-}
+import { PlayerService } from '../../services/player.service';
+import { Player } from '../../models/Player';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [RouterLink,AsyncPipe,NgIf],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  inventory: Item[] = [
-    { name: 'Sword', img: '../../assets/avatar.jpeg', slot: 'weapon' },
-    { name: 'Shield', img: '../../assets/avatar.jpeg', slot: 'offhand' },
-    { name: 'Health Potion', img: '../../assets/avatar.jpeg', slot: 'potion' },
-    { name: 'Mana Potion', img: '../../assets/avatar.jpeg', slot: 'potion' },
-    { name: 'Gold', img: '../../assets/avatar.jpeg', slot: 'misc' }
-  ];
+export class HomeComponent{
+  player$!: Observable<Player>;
 
-  equipmentSlots: EquipmentSlot[] = [
-    { name: 'Weapon', item: null },
-    { name: 'Offhand', item: null },
-    { name: 'Armor', item: null },
-    { name: 'Potion', item: null },
-    { name: 'Misc', item: null }
-  ];
+  constructor(private playerService : PlayerService){}
 
-  equipItem(item: Item) {
-    const slot = this.equipmentSlots.find(s => s.name.toLowerCase() === item.slot);
-    if (slot) {
-      slot.item = item;
-      this.inventory = this.inventory.filter(i => i !== item);
-    }
-  }
-
-  unequipItem(slot: EquipmentSlot) {
-    if (slot.item) {
-      this.inventory.push(slot.item);
-      slot.item = null;
-    }
+  ngOnInit(): void {
+    this.player$ = this.playerService.getPlayer()
   }
 }
